863bc93d6df0df00051b853c9906ce1314aa8ca4
Edson <edson0195@gmail.com>
2025-06-18 18:38:19 -0300

    feat: add login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import TaskPage from './pages/TaskPage.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import TaskPage from './pages/TaskPage.jsx';
+import LoginPage from './login/page.js';
+
+// Importa sua chave publicável Clerk do .env
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 const router = createBrowserRouter([
   {
@@ -13,11 +22,17 @@ const router = createBrowserRouter([
   {
     path: "/task",
     element: <TaskPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   }
 ]);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </React.StrictMode>
 );
