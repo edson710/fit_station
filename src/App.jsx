b4f79589543384fd26dca9d6e6fcd3d2c6fa8077
Edson Nonato <edson0195@gmail.com>
2025-06-18 19:14:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,6 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  useEffect(() => {
-    const fetchTasks = async () => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10", {
-        method: "GET",
-      });
-      const data = await response.json();
-      setTasks(data);
-    };
-    fetchTasks();
-  }, []);
-
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       if (task.id === taskId) {
@@ -38,20 +27,17 @@ function App() {
   }
 
   function onAddTaskSubmit(title, description) {
-    const newTasks = {
+    const newTask = {
       id: v4(),
       title,
       description,
       isCompleted: false,
     };
-    setTasks([...tasks, newTasks]);
+    setTasks([...tasks, newTask]);
   }
 
   return (
     <div className="w-screen min-h-screen bg-slate-500 flex flex-col items-center p-6">
-      {/* Cabeçalho Clerk */}
-      <header className="w-full flex justify-end items-center mb-4">
-
       <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
